88d700a506bec2c2655cbb72946dcc6316f3d8bf
Hannah Cole <hannahcole@fb.com>
2019-07-11 16:12:45 -0700

    added to detail view

--- app/src/main/java/com/example/instantgrandma/PostDetailActivity.java
+++ app/src/main/java/com/example/instantgrandma/PostDetailActivity.java
@@ -8,11 +8,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.bumptech.glide.Glide;
 import com.example.instantgrandma.models.Post;
+import com.parse.ParseFile;
 
 public class PostDetailActivity extends AppCompatActivity {
     TextView tvDescription;
     ImageView ivImage;
     TextView tvTime;
+    TextView tvUser;
+    ImageView ivProPic;
     Post post;
 
     @Override
@@ -22,6 +25,8 @@ public class PostDetailActivity extends AppCompatActivity {
         tvDescription = findViewById(R.id.tvDetailDescription);
         ivImage = findViewById(R.id.ivDetail);
         tvTime = findViewById(R.id.tvDetailTime);
+        tvUser = findViewById(R.id.tvDetailUser);
+        ivProPic = findViewById(R.id.ivDetailProPic);
 
         post = (Post) getIntent().getExtras().get(Post.class.getSimpleName());
 
@@ -29,8 +34,12 @@ public class PostDetailActivity extends AppCompatActivity {
         if (post.getCreatedAt() != null) {
             tvTime.setText(post.getCreatedAt().toString());
         }
+        tvUser.setText(post.getUser().getUsername());
 
-
+        ParseFile image = (ParseFile) post.getUser().get("image");
+        if (image != null) {
+            Glide.with(this).load(image.getUrl()).into(ivProPic);
+        }
         Glide.with(this).load(post.getImage().getUrl()).into(ivImage);
     }
 }
