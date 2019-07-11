67c06119e48dccd1751257af55b021e36c8f26bd
Hannah Cole <hannahcole@fb.com>
2019-07-11 15:52:11 -0700

    propic

--- app/src/main/java/com/example/instantgrandma/fragments/ProfileFragment.java
+++ app/src/main/java/com/example/instantgrandma/fragments/ProfileFragment.java
@@ -1,23 +1,51 @@
 package com.example.instantgrandma.fragments;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
 
+import com.bumptech.glide.Glide;
 import com.example.instantgrandma.LogInActivity;
 import com.example.instantgrandma.R;
+import com.parse.ParseException;
+import com.parse.ParseFile;
 import com.parse.ParseUser;
+import com.parse.SaveCallback;
+
+import java.io.File;
+
+import static android.app.Activity.RESULT_OK;
+import static android.widget.Toast.LENGTH_SHORT;
 
 public class ProfileFragment extends Fragment {
 
+    private static final String TAG = ProfileFragment.class.getSimpleName();
+
     Button btnLogout;
+    Button btnChangeProPic;
+    Button btnSaveProfile;
+    ImageView ivPropic;
+    TextView tvUsername;
+    private File photoFile;
+    public final static int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 1034;
+    public String photoFileName = "photo.jpg";
 
 
     public ProfileFragment() {
@@ -35,7 +63,14 @@ public class ProfileFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        tvUsername = view.findViewById(R.id.tvUsername);
+        ivPropic = view.findViewById(R.id.ivProPic);
         btnLogout = view.findViewById(R.id.btnLogout);
+        btnChangeProPic = view.findViewById(R.id.btnChangeProPic);
+        btnSaveProfile = view.findViewById(R.id.btnSaveProfile);
+        tvUsername.setText(ParseUser.getCurrentUser().getUsername());
+        ParseFile image = (ParseFile) ParseUser.getCurrentUser().get("image");
+        Glide.with(getContext()).load(image.getUrl()).into(ivPropic);
         btnLogout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -45,6 +80,95 @@ public class ProfileFragment extends Fragment {
                 startActivity(i);
             }
         });
+
+        btnChangeProPic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launchCamera();
+            }
+        });
+
+        btnSaveProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ParseUser user = ParseUser.getCurrentUser();
+                if (photoFile == null || ivPropic.getDrawable() == null) {
+                    Toast.makeText(getContext(), "No photo!", LENGTH_SHORT).show();
+                    return;
+                }
+                savePost(ParseUser.getCurrentUser(), photoFile);
+
+            }
+        });
+    }
+
+    private void launchCamera() {
+        // create Intent to take a picture and return control to the calling application
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Create a File reference to access to future access
+        photoFile = getPhotoFileUri(photoFileName);
+
+        // wrap File object into a content provider
+        // required for API >= 24
+        // See https://guides.codepath.com/android/Sharing-Content-with-Intents#sharing-files-with-api-24-or-higher
+        Uri fileProvider = FileProvider.getUriForFile(getContext(), "com.codepath.fileprovider", photoFile);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider);
+
+        // If you call startActivityForResult() using an intent that no app can handle, your app will crash.
+        // So as long as the result is not null, it's safe to use the intent.
+        if (intent.resolveActivity(getContext().getPackageManager()) != null) {
+            // Start the image capture intent to take photo
+            startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+        }
+    }
+
+    // Returns the File for a photo stored on disk given the fileName
+    public File getPhotoFileUri(String fileName) {
+        // Get safe storage directory for photos
+        // Use `getExternalFilesDir` on Context to access package-specific directories.
+        // This way, we don't need to request external read/write runtime permissions.
+        File mediaStorageDir = new File(getContext().getExternalFilesDir(Environment.DIRECTORY_PICTURES), TAG);
+
+        // Create the storage directory if it does not exist
+        if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()){
+            Log.d(TAG, "failed to create directory");
+        }
+
+        // Return the file target for the photo based on filename
+        File file = new File(mediaStorageDir.getPath() + File.separator + fileName);
+
+        return file;
+    }
+
+    private void savePost(ParseUser user, File photoFile) {
+
+        user.put("image", new ParseFile(photoFile));
+        user.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e != null) {
+                    Log.d(TAG, "Post wasn't saved");
+                } else {
+                    Log.d(TAG, "Success");
+                }
+
+            }
+        });
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                // by this point we have the camera photo on disk
+                Bitmap takenImage = BitmapFactory.decodeFile(photoFile.getAbsolutePath());
+                // RESIZE BITMAP, see section below
+                // Load the taken image into a preview
+                ivPropic.setImageBitmap(takenImage);
+            } else { // Result was a failure
+                Toast.makeText(getContext(), "Picture wasn't taken!", LENGTH_SHORT).show();
+            }
+        }
     }
 
     @Override
